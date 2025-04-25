8b5aade5ce99e3c57e21f77351f900818555df89
PelerYuan <pelerlittlepig@gmail.com>
2025-04-25 20:33:46 +0800

    review entrance

--- app.py
+++ app.py
@@ -13,9 +13,14 @@ def index():
             with open(f'data/quizs/{quiz_name}', 'r', encoding='utf-8') as f:
                 quiz = json.loads(f.read())
                 tests.append({'name':quiz_name[:-5], 'title': quiz['title'], 'subtitle': quiz['subtitle']})
-        return render_template('index.html', user_name=session['name'], tests=tests)
+        return render_template('index.html', class_=session['class'],name=session['name'], tests=tests)
 
     return redirect(url_for('login'))
+
+def is_tested(quiz_name):
+    print(f"data/students/{session['class']}/{quiz_name}_{session['name']}.json")
+    return os.path.exists(f"data/students/{session['class']}/{quiz_name}_{session['name']}.json")
+app.jinja_env.globals['is_tested'] = is_tested
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -40,17 +45,33 @@ def quiz(quiz_name):
     with open(f'data/quizs/{quiz_name}.json', 'r', encoding='utf-8') as f:
         quiz = json.loads(f.read())
         for i in range(len(quiz['questions'])):
-            question = quiz['questions'][i]['index'] = i + 1
+            quiz['questions'][i]['index'] = i + 1
 
-    return render_template('quiz.html', quiz=quiz)
+    return render_template('quiz.html', quiz=quiz, quiz_name=quiz_name)
 
 
-@app.route('/submit', methods=['POST'])
-def correct():
+@app.route('/submit/<quiz_name>', methods=['POST'])
+def submit(quiz_name):
     """
-    Check correct answer
+    Record submit
     """
-    print(request.form)
+    selection = {}
+    for key in request.form.keys():
+        selection[int(key)] = request.form.getlist(key)
+
+    with open(f"data/students/{session['class']}/{quiz_name}_{session['name']}.json", 'w', encoding='utf-8') as f:
+        json.dump(selection, f, ensure_ascii=False, indent=4)
+    return "Hello"
+
+@app.route('/review/<quiz_name>')
+def review(quiz_name):
+    with open(f'data/quizs/{quiz_name}.json', 'r', encoding='utf-8') as f:
+        quiz = json.loads(f.read())
+        for i in range(len(quiz['questions'])):
+            quiz['questions'][i]['index'] = i + 1
+    with open(f"data/students/{session['class']}/{quiz_name}_{session['name']}.json", 'r', encoding='utf-8') as f:
+        answer = json.loads(f.read())
+    return render_template('review.html', quiz=quiz, answer=answer)
 
 
 @app.route('/test')
