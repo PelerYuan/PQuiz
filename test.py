cf026f1408b699b11c80e4e9f158bd8dc1b0e410
PelerYuan <pelerlittlepig@gmail.com>
2025-04-28 10:05:58 +0800

    delete test

--- test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import json
-
-with open('data/students/Commerce/quizU1_a.json', 'r', encoding='utf-8') as f:
-    data = json.loads(f.read())
-    for i in range(100):
-        with open(f'data/students/Commerce/quizU1_{i}.json', 'w', encoding='utf-8') as g:
-            json.dump(data, g)
-            print(i)
